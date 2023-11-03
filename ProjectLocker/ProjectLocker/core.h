4eb1eda238cfd2186b46408f17c4a9f6fb2f10a0
らいっく <128302647+NCZLL@users.noreply.github.com>
2023-11-03 15:57:58 +0900

    Update

---
+++ ProjectLocker/ProjectLocker/core.h
@@ -0,0 +1,272 @@
+#pragma once
+#include "ImGui\imgui.h"
+#include "ImGui\imgui_impl_dx11.h"
+#include "ImGui\imgui_impl_win32.h"
+#include "Utils\Memory\Memory.h"
+#include "Utils\SimpleMath\SimpleMath.h"
+#include <thread>
+#include <dwmapi.h>
+#include <string>
+#include <d3d11.h>
+#include <vector>
+#pragma comment(lib, "d3d11.lib")
+using namespace DirectX::SimpleMath;
+
+struct AxisAlignedBox
+{
+    Vector4 Min;
+    Vector4 Max;
+};
+
+class Cheat
+{
+private:
+    // ESP Setting
+    bool v_VehicleESP = true;
+    bool v_TeamESP = false;
+
+    // ESP Options
+    bool v_Box = true;
+    bool v_BoxFilled = true;
+    bool v_Line = false;
+    bool v_Bone = true;
+    bool v_Health = true;
+    bool v_Distance = true;
+    float MaxDistance = 700.f;
+
+    float BoneLine = 1;
+
+    // Misc
+    bool Alive = false;
+    bool SwayEditor = false;
+    float SwayValue = 0.935f;
+    bool UnlockAll = false;
+    //bool DamageHack = false;
+
+    // ESP Color
+    ImVec4 color_Normal = { 1.f, 0.f, 0.f, 1.f };
+    ImVec4 color_Visible = { 0.f, 1.f, 0.f, 1.f };
+    ImVec4 color_Team = { 0.f, 1.f, 1.f, 1.f };
+
+    ImVec4 color_bone = { 1.f, 1.f, 1.f, 1.f };
+
+    std::vector<std::string>NameList;
+public:
+    // Window
+    char ClassName[128];
+    char MenuName[128];
+    RECT GameSize;
+    POINT GamePOINT;
+    
+    // Global
+    bool m_ESP = true;
+    bool VSync = true;
+
+    // Function
+    void Info();
+    void SpectatorList();
+    void Menu();
+    void ESP();
+    void FuncMisc();
+    void DrawAABB(AxisAlignedBox aabb, Vector3 m_Position, float Yaw, ImColor color);
+    void DrawAABB(AxisAlignedBox aabb, Matrix tranform, ImColor color);
+
+    void DrawLine(ImVec2 b, ImVec2 f, ImColor color, float w)
+    {
+        ImGui::GetWindowDrawList()->AddLine(b, f, color, w);
+    }
+    void DrawLineEx(Vector3 a, Vector3 b, ImColor color)
+    {
+        ImGui::GetWindowDrawList()->AddLine(ImVec2(a.x, a.y), ImVec2(b.x, b.y), color, 1.f);
+    }
+    void SimpleBox(ImColor color, float x, float y, float w, float h)
+    {
+        DrawLine(ImVec2(x, y), ImVec2(x + w, y), color, 1.0f);
+        DrawLine(ImVec2(x, y), ImVec2(x, y + h), color, 1.0f);
+        DrawLine(ImVec2(x + w, y), ImVec2(x + w, y + h), color, 1.0f);
+        DrawLine(ImVec2(x, y + h), ImVec2(x + w, y + h), color, 1.0f);
+    }
+    void DrawBox(int x, int y, int w, int h, ImColor color, ImColor BorderColor)
+    {
+        SimpleBox(color, x, y, w, h);
+
+        /*
+            Box Border. Fail.
+
+        DrawLine(ImVec2(x - 1, y - 1), ImVec2(x + w + 1, y - 1), BorderColor, 1.0f);
+        DrawLine(ImVec2(x + 1, y + 1), ImVec2(x + w - 1, y + 1), BorderColor, 1.0f);
+
+        DrawLine(ImVec2(x - 1, y - 1), ImVec2(x - 1, y + h + 1), BorderColor, 1.0f);
+        DrawLine(ImVec2(x + 1, y + 2), ImVec2(x + 1, y + h - 1), BorderColor, 1.0f);
+
+        DrawLine(ImVec2(x + w - 1, y + 1), ImVec2(x + w - 1, y + h - 1), BorderColor, 1.0f);
+        DrawLine(ImVec2(x + w + 1, y - 1), ImVec2(x + w + 1, y + h + 2), BorderColor, 1.0f);
+
+        DrawLine(ImVec2(x + 1, y + h - 1), ImVec2(x + w, y + h - 1), BorderColor, 1.0f);
+        DrawLine(ImVec2(x - 1, y + h + 1), ImVec2(x + w + 1, y + h + 1), BorderColor, 1.0f);
+        */
+    }
+    void RectFilled(float x0, float y0, float x1, float y1, ImColor color, float rounding, int rounding_corners_flags)
+    {
+        ImGui::GetWindowDrawList()->AddRectFilled(ImVec2(x0, y0), ImVec2(x1, y1), color, rounding, rounding_corners_flags);
+    }
+    void HealthBar(float x, float y, float w, float h, int value, int v_max, bool Background)
+    {
+        ImColor barColor = ImColor(min(510 * (v_max - value) / 100, 255), min(510 * value / 100, 255), 25, 255);
+
+        if (value < 0.f)
+            value = 0.f;
+
+        // BaseBar
+        if (Background)
+            RectFilled(x - 1, y + 1, x + w + 1, y + h - 1, ImColor(0.f, 0.f, 0.f, 0.6f), 0.f, 0);
+
+        RectFilled(x, y, x + w, y + ((h / float(v_max)) * (float)(value <= 0.f ? 0 : value)), barColor, 0.0f, 0);
+    }
+    void Text(ImVec2 pos, ImColor color, const char* text_begin, const char* text_end, float wrap_width, const ImVec4* cpu_fine_clip_rect)
+    {
+        ImGui::GetWindowDrawList()->AddText(ImGui::GetFont(), ImGui::GetFontSize(), pos, color, text_begin, text_end, wrap_width, cpu_fine_clip_rect);
+    }
+    void String(ImVec2 pos, ImColor color, const char* text)
+    {
+        Text(pos, color, text, text + strlen(text), 200, 0);
+    }
+};
+
+static ID3D11Device* g_pd3dDevice = NULL;
+static ID3D11DeviceContext* g_pd3dDeviceContext = NULL;
+static IDXGISwapChain* g_pSwapChain = NULL;
+static ID3D11RenderTargetView* g_mainRenderTargetView = NULL;
+
+static void CleanupRenderTarget()
+{
+    if (g_mainRenderTargetView) { g_mainRenderTargetView->Release(); g_mainRenderTargetView = NULL; }
+}
+
+static void CleanupDeviceD3D()
+{
+    CleanupRenderTarget();
+    if (g_pSwapChain) { g_pSwapChain->Release(); g_pSwapChain = NULL; }
+    if (g_pd3dDeviceContext) { g_pd3dDeviceContext->Release(); g_pd3dDeviceContext = NULL; }
+    if (g_pd3dDevice) { g_pd3dDevice->Release(); g_pd3dDevice = NULL; }
+}
+
+static void CreateRenderTarget()
+{
+    ID3D11Texture2D* pBackBuffer;
+    g_pSwapChain->GetBuffer(0, IID_PPV_ARGS(&pBackBuffer));
+    g_pd3dDevice->CreateRenderTargetView(pBackBuffer, NULL, &g_mainRenderTargetView);
+    pBackBuffer->Release();
+}
+
+static bool CreateDeviceD3D(HWND hWnd)
+{
+    DXGI_SWAP_CHAIN_DESC sd;
+    ZeroMemory(&sd, sizeof(sd));
+    sd.BufferCount = 2;
+    sd.BufferDesc.Width = 0;
+    sd.BufferDesc.Height = 0;
+    sd.BufferDesc.Format = DXGI_FORMAT_R8G8B8A8_UNORM;
+    sd.BufferDesc.RefreshRate.Numerator = 60;
+    sd.BufferDesc.RefreshRate.Denominator = 1;
+    sd.Flags = DXGI_SWAP_CHAIN_FLAG_ALLOW_MODE_SWITCH;
+    sd.BufferUsage = DXGI_USAGE_RENDER_TARGET_OUTPUT;
+    sd.OutputWindow = hWnd;
+    sd.SampleDesc.Count = 1;
+    sd.SampleDesc.Quality = 0;
+    sd.Windowed = TRUE;
+    sd.SwapEffect = DXGI_SWAP_EFFECT_DISCARD;
+
+    UINT createDeviceFlags = 0;
+    D3D_FEATURE_LEVEL featureLevel;
+    const D3D_FEATURE_LEVEL featureLevelArray[2] = { D3D_FEATURE_LEVEL_11_0, D3D_FEATURE_LEVEL_10_0, };
+    if (D3D11CreateDeviceAndSwapChain(NULL, D3D_DRIVER_TYPE_HARDWARE, NULL, createDeviceFlags, featureLevelArray, 2, D3D11_SDK_VERSION, &sd, &g_pSwapChain, &g_pd3dDevice, &featureLevel, &g_pd3dDeviceContext) != S_OK)
+        return false;
+
+    CreateRenderTarget();
+    return true;
+}
+
+extern IMGUI_IMPL_API LRESULT ImGui_ImplWin32_WndProcHandler(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam);
+
+static LRESULT WINAPI WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam)
+{
+    if (ImGui_ImplWin32_WndProcHandler(hWnd, msg, wParam, lParam))
+        return true;
+
+    switch (msg)
+    {
+    case WM_SIZE:
+        if (g_pd3dDevice != NULL && wParam != SIZE_MINIMIZED)
+        {
+            CleanupRenderTarget();
+            g_pSwapChain->ResizeBuffers(0, (UINT)LOWORD(lParam), (UINT)HIWORD(lParam), DXGI_FORMAT_UNKNOWN, 0);
+            CreateRenderTarget();
+        }
+        return 0;
+    case WM_SYSCOMMAND:
+        if ((wParam & 0xfff0) == SC_KEYMENU)
+            return 0;
+        break;
+    case WM_DESTROY:
+        PostQuitMessage(0);
+        return 0;
+    }
+    return DefWindowProcA(hWnd, msg, wParam, lParam);
+}
+
+// ImGui Style.
+static void ImStyle()
+{
+    ImGui::StyleColorsClassic();
+    ImGuiStyle* style = &ImGui::GetStyle();
+
+    style->WindowPadding = ImVec2(15, 15);
+    style->WindowRounding = 0.f;
+    style->FramePadding = ImVec2(5, 5);
+    style->FrameRounding = 2.0f;
+    style->ItemSpacing = ImVec2(12, 8);
+    style->ItemInnerSpacing = ImVec2(8, 6);
+    style->IndentSpacing = 25.0f;
+    style->ScrollbarSize = 15.0f;
+    style->ScrollbarRounding = 9.0f;
+    style->GrabMinSize = 5.0f;
+    style->GrabRounding = 3.0f;
+    style->WindowTitleAlign.x = 0.50f;
+    style->SeparatorTextBorderSize = 1.f;
+
+    style->Colors[ImGuiCol_Text] = ImVec4(0.80f, 0.80f, 0.83f, 1.00f);
+    style->Colors[ImGuiCol_TextDisabled] = ImVec4(0.24f, 0.23f, 0.29f, 1.00f);
+    style->Colors[ImGuiCol_WindowBg] = ImVec4(0.06f, 0.05f, 0.07f, 1.00f);
+    style->Colors[ImGuiCol_PopupBg] = ImVec4(0.07f, 0.07f, 0.09f, 1.00f);
+    style->Colors[ImGuiCol_Border] = ImVec4(0.80f, 0.80f, 0.83f, 0.28f);
+    style->Colors[ImGuiCol_BorderShadow] = ImVec4(0.92f, 0.91f, 0.88f, 0.00f);
+    style->Colors[ImGuiCol_FrameBg] = ImVec4(0.10f, 0.09f, 0.12f, 1.00f);
+    style->Colors[ImGuiCol_FrameBgHovered] = ImVec4(0.24f, 0.23f, 0.29f, 1.00f);
+    style->Colors[ImGuiCol_FrameBgActive] = ImVec4(0.56f, 0.56f, 0.58f, 1.00f);
+    style->Colors[ImGuiCol_TitleBg] = ImVec4(0.10f, 0.09f, 0.12f, 1.00f);
+    style->Colors[ImGuiCol_TitleBgCollapsed] = ImVec4(1.00f, 0.98f, 0.95f, 0.75f);
+    style->Colors[ImGuiCol_TitleBgActive] = ImVec4(0.07f, 0.07f, 0.09f, 1.00f);
+    style->Colors[ImGuiCol_MenuBarBg] = ImVec4(0.10f, 0.09f, 0.12f, 1.00f);
+    style->Colors[ImGuiCol_ScrollbarBg] = ImVec4(0.10f, 0.09f, 0.12f, 1.00f);
+    style->Colors[ImGuiCol_ScrollbarGrab] = ImVec4(0.80f, 0.80f, 0.83f, 0.31f);
+    style->Colors[ImGuiCol_ScrollbarGrabHovered] = ImVec4(0.56f, 0.56f, 0.58f, 1.00f);
+    style->Colors[ImGuiCol_ScrollbarGrabActive] = ImVec4(0.06f, 0.05f, 0.07f, 1.00f);
+    style->Colors[ImGuiCol_CheckMark] = ImVec4(0.80f, 0.80f, 0.83f, 0.31f);
+    style->Colors[ImGuiCol_SliderGrab] = ImVec4(0.80f, 0.80f, 0.83f, 0.31f);
+    style->Colors[ImGuiCol_SliderGrabActive] = ImVec4(0.06f, 0.05f, 0.07f, 1.00f);
+    style->Colors[ImGuiCol_Button] = ImVec4(0.10f, 0.09f, 0.12f, 1.00f);
+    style->Colors[ImGuiCol_ButtonHovered] = ImVec4(0.24f, 0.23f, 0.29f, 1.00f);
+    style->Colors[ImGuiCol_ButtonActive] = ImVec4(0.56f, 0.56f, 0.58f, 1.00f);
+    style->Colors[ImGuiCol_Header] = ImVec4(0.10f, 0.09f, 0.12f, 1.00f);
+    style->Colors[ImGuiCol_HeaderHovered] = ImVec4(0.56f, 0.56f, 0.58f, 1.00f);
+    style->Colors[ImGuiCol_HeaderActive] = ImVec4(0.06f, 0.05f, 0.07f, 1.00f);
+    style->Colors[ImGuiCol_ResizeGrip] = ImVec4(0.00f, 0.00f, 0.00f, 0.00f);
+    style->Colors[ImGuiCol_ResizeGripHovered] = ImVec4(0.56f, 0.56f, 0.58f, 1.00f);
+    style->Colors[ImGuiCol_ResizeGripActive] = ImVec4(0.06f, 0.05f, 0.07f, 1.00f);
+    style->Colors[ImGuiCol_PlotLines] = ImVec4(0.40f, 0.39f, 0.38f, 0.63f);
+    style->Colors[ImGuiCol_PlotLinesHovered] = ImVec4(0.25f, 1.00f, 0.00f, 1.00f);
+    style->Colors[ImGuiCol_PlotHistogram] = ImVec4(0.40f, 0.39f, 0.38f, 0.63f);
+    style->Colors[ImGuiCol_PlotHistogramHovered] = ImVec4(0.25f, 1.00f, 0.00f, 1.00f);
+    style->Colors[ImGuiCol_TextSelectedBg] = ImVec4(0.25f, 1.00f, 0.00f, 0.43f);
+}
